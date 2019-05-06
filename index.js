60925c871629e6d07af9053ee2c49f4580a598ef
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2019-05-06 13:01:58 -0400

    Interim checkin

---
+++ index.js
@@ -0,0 +1,11 @@
+function produceDrivingRange(num) {
+  return function(blockRange) {
+   return blockRange - num 
+  }
+  }
+
+function produceTipCalculator() {
+  return function () {
+    
+  }
+}
