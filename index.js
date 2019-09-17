f671bf24862ab1c756f9d13751e779bda8120d13
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2019-09-17 16:51:24 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function produceDrivingRange(blockRange) {
+  return function(beginningBlock, endingBlock) {
+    const numberBlocks = parseInt(endingBlock) - parseInt(beginningBlock)
+    const blockRangeDifference = blockRange - numberBlocks
+    if (numberBlocks >= blockRange) {
+      return `${Math.abs(blockRangeDifference)} blocks out of range`
+    } else {
+      return `within range by ${blockRangeDifference}`
+    }
+  }
+};
+
+function produceTipCalculator(percentTip) {
+  return function(ridePrice) {
+    return ridePrice * percentTip
+  }
+}
+
+function createDriver() {
+  let driverId = 0
+  return class {
+    constructor( name ) {
+      this.name = name
+      this.id = ++driverId
+    }
+  }
+}
